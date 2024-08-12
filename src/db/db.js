aca05bf53d4685bfea6e83b78a7a89d6c956785f
Abdullahata450 <abdullahbinata450@gmail.com>
2024-08-12 11:31:05 +0500

    MongoDB connection

---
+++ src/db/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const ConnectDB = async () => {
+    try {
+        console.log(`Connecting to MongoDB with URI: ${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected: DB HOST ${connection.connection.host}`);
+    } catch (error) {
+        console.error("Mongoose Error:", error.message);
+        process.exit(1);
+    }
+};
+
+
+
+export default ConnectDB;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ConnectDB from "./db/db.js";
+import dotenv from  "dotenv"
+dotenv.config()
+// dotenv.config(
+//     {
+//         path: './env'
+//     }
+// )
+
+
+
+ConnectDB()
