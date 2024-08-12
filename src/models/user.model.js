e996ed9d638de7d2b0d8b481e096e40b1a21215e
Abdullahata450 <abdullahbinata450@gmail.com>
2024-08-12 14:12:59 +0500

    Added Models and Use of Bcrypt and JWT

---
+++ src/models/user.model.js
@@ -0,0 +1,82 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+
+{
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        index:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trime:true,
+    },
+    fullname:{
+         type:String,
+         required:true,
+         trim:true,
+    },
+    avatar:{
+        type:String,
+        required:true,
+    },
+    coverImage:{
+        type:String,
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video",
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,"Please provide password"], 
+    },
+    refreshToken:{
+        type:String,
+    }
+
+
+},
+{
+    timestamps:true,
+}
+
+);
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")){                                 // save the user password in encrpeted form
+        this.password=bcrypt.hash(this.password,10)
+        next();
+    }
+    else next();
+})
+userSchema.methods.isPasswordisCorrect = async function(password) {
+   return await bcrypt.compare(password,this.password);                       // if the user enter the password it will check the password form database which is in encrpeyted form and compare it with the user password
+}
+
+userSchema.methods.generateAccessToken = function() {
+   return  jwt.sign({
+        _id:this._id,                           // this is payload
+        email:this.email,
+        password:this.password,
+        fullname:this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function() {}
+
+export const User = mongoose.model("User",userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+    
+    {
+       videoFile:{
+        type: String,
+        required:true,
+       },
+       thumbnail:{
+        type: String,
+        required:true,
+       },
+       title:{
+        type: String,
+        required:true,
+       },
+       description:{
+        type: String,
+        required:true,
+       },
+       duration:{
+        type:Number,
+        required:true,
+       },
+       views:{
+        type:Number,
+        default:0,
+       },
+       isPublished:{
+        type:Boolean,
+        default:True,
+       },
+       owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+       }
+    },
+    
+    {
+        timestamps:true
+    }
+)
+
+
+export const Video = mongoose.model("Video",videoSchema)
